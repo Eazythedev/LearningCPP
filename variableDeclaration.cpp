aad2f3ccb157d9e7ca04ac8910ed0abfb345bb49
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2025-03-10 15:45:37 -0700

    Create variableDeclaration.cpp

---
+++ variableDeclaration.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int catCount;
+    catCount = 10;
+    cout << "Cat Count: " << catCount << ".\n";
+
+    double displacement;
+    displacement = 2.4;
+    cout << "Displacement: " << displacement << "L.\n";
+
+    char initial;
+    initial = 'E';
+    cout << "Initial: " << initial << ".\n";
+
+    bool isOn;
+    isOn = true;
+    cout << "The light is on: " << isOn << ".\n";
+
+    string planet;
+    planet = "Reach";
+    cout << "Planet: " << planet << ".";
+
+}   
