5ba1d82e5aa08ed3193375bce963fbc525d476ba
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2025-03-13 14:49:21 -0700

    Create RPGGreeting.cpp

---
+++ RPGGreeting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string greeting;
+    cout << "Kazuo says \'Ohayou Gozaimasu.\'\nResponse: ";
+    cin >> greeting;
+    cout << "You respond \'" << greeting << ".\'";
+}   
