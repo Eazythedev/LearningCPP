d6394f7335640a700b6a4b293961dcdeecf06753
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2025-03-14 11:25:08 -0700

    Create signedInt.cpp

---
+++ signedInt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Printing the size of a normal and a signed int
+    cout << "signed int size: " << sizeof(signed int)
+        << " bytes." << "\n";
+    cout << "int size: " << sizeof(int) << " bytes";
+
+    return 0;
+}   
