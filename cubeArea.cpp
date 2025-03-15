10e2e7e5480e0530fcae25b6b9cb0c36c3a4f78f
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2025-03-15 10:33:23 -0700

    Create cubeArea.cpp

---
+++ cubeArea.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int cubeSide = 7;
+    int area;
+
+    area = cubeSide * cubeSide;
+    cout << "The area of the cube is: " << area << " .";
+
+    return 0;
+}   
