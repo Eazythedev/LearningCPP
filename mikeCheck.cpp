1411121ca0dfb2e479820c9ca807d827ac8547d3
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2025-03-22 20:47:23 -0700

    Update mikeCheck.cpp
    A program that checks if you are MJ if you type that your name is 'Michael' or 'Mike'.

--- mikeCheck.cpp
+++ mikeCheck.cpp
@@ -1,14 +1,15 @@
 //
-//  mikeCheck.cpp
-//  MikeCheck
+//  main.cpp
+//  LearningC++
 //
-//  Created by Ezaiah Akyeem on March 22, 2025.
+//  Created by Ezaiah Akyeem on 11/1/23. (This code was written in this file in this project on March 22, 2025 (3/22/25))
 //
 // The Mike Check
 
 // Mike Checker (mikeCheck.cpp)
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
     string name;
