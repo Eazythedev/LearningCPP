ee87eb5cdac20db943b4a657130ec55ef32bf59e
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2025-09-22 15:53:53 -0700

    Create lambdaFunctionGuessingGame.cpp
    A guessing game using a lambda function.

---
+++ lambdaFunctionGuessingGame.cpp
@@ -0,0 +1,32 @@
+//
+// Main.cpp
+// Learningcpp
+//
+// Created by Ezaiah Akyeem/Eazythedev 
+// September 22, 2025
+// Labmda Function Guessing Game
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    auto correct = []() {
+        cout << "Correct!" << "\n";
+    };
+
+    int num = 5;
+    int guess;
+
+    cout << "Guess a number between 1 and 10: ";
+    cin >> guess;
+
+    if (guess == num) {
+        correct();
+    }
+    else {
+        cout << "Incorrect." << "\n";
+    }
+    return 0;
+}
