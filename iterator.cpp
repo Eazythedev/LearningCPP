c21752938b69317209986fc154d83744409607cf
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2026-01-26 06:41:53 -0800

    Update iterator.cpp

--- iterator.cpp
+++ iterator.cpp
@@ -8,7 +8,7 @@
 
 // Vector called 'cars' that will store strings
     vector<string> cars = { "Viper", "Corvette", "Camaro", "Mustang" };
-    //Create a vector iterator called it (for iterator)
+    // Create a vector iterator called it (for iterator)
     vector<string>::iterator it;
     // Loop through the vector with the iterator
     for (it = cars.begin(); it != cars.end(); ++it) {
