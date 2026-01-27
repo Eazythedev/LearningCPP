e2d6a101bc8192b0400f1459a738661f21b5bff2
Eazythedev <111327362+Eazythedev@users.noreply.github.com>
2026-01-27 07:07:46 -0800

    Update iterator.cpp

--- iterator.cpp
+++ iterator.cpp
@@ -7,6 +7,12 @@
 // A Vector and an Iterator
 
 // Vector called 'cars' that will store strings
+
+#inclue <iostream>
+#include <string>
+using namespace std;
+
+int main() {
     vector<string> cars = { "Viper", "Corvette", "Camaro", "Mustang" };
     // Create a vector iterator called it (for iterator)
     vector<string>::iterator it;
@@ -14,3 +20,19 @@
     for (it = cars.begin(); it != cars.end(); ++it) {
         cout << *it << "\n";
     }
+
+    vector<string> cars2 = { "Viper", "Corvette", "Camaro", "Mustang" };
+
+    for (auto it = cars.begin(); it != cars.end(); ) {
+        if (*it == "BMW") {
+            it = cars.erase(it);
+        }
+        else {
+            ++it;
+        }
+    }
+
+    for (const string& car2 : cars2) {
+        cout << car2 << "\n";
+    }
+}
