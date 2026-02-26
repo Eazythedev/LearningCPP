457a90dd13d850056acd1016a6e25db93ff41a77
Eazy <111327362+Eazythedev@users.noreply.github.com>
2026-02-26 05:04:40 -0800

    Create JPVehicleCategorizer.cpp

---
+++ JPVehicleCategorizer.cpp
@@ -0,0 +1,27 @@
+//
+// JPVehicleCategorizer.cpp
+// Japanese Vehicle Catgorizer
+//
+// Created by Ezaiah AKyeem / Eazythedev / Erujine
+// Date Created: February 26, 2026
+// A categorizing Program that organzies Japanese model vehicles by their class and specifications.
+
+#include <iostream>
+using namespace std;
+
+void JPVehicle(string make, string model, int modelYear, string vehicleClass, int engine_size) {
+    // 
+}
+
+int main()
+{
+    cout << "Welcome to the vehicle categorizer.";
+
+    cout << "Enter vehicle make: ";
+    cin >> JPVehicle.make;
+
+    cout << "Enter vehicle model: ";
+    cout <<  "Enter vehicle model year: ";
+    cout << "Enter vehicle class: ";
+    cout << "Enter vehicle engine size: ";
+}
