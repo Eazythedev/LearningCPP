02002b9baf1fe48a60cff88225cdbc024328ba43
Eazy <111327362+Eazythedev@users.noreply.github.com>
2026-03-01 06:40:55 -0800

    Update carClassifierJP.cpp

--- carClassifierJP.cpp
+++ carClassifierJP.cpp
@@ -6,6 +6,8 @@
 // Date Created: February 27, 2026
 // A program that takes user input to categorize a vehicle.
 
+// Updated: March 1, 2026
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -35,6 +37,14 @@ int main() {
     cout << "Enter vehicle engine size: ";
     cin >> carObj1.engineSize;
 
+    if (carObj1.modelYear < 1986) {
+        cout << "This is a vintage car." << "\n";
+    }
+    else if (carObj1.modelYear > 1986 & carObj1.modelYear < 2006) {
+        cout << "This is an older model car." << "\n";
+    }
+    else if (carObj1.modelYear > 2006 & carObj1.modelYear < 2020) {
+        cout << "This is a modern car." << "\n";
 
     if (carObj1.engineSize <= 660) {
         cout << "This is a kei-car.";
